abad3a8bf7f47db1a2e7452b4b9436eb22a7e64d
Robert Pedersen <rob84044@users.noreply.github.com>
2019-05-09 18:53:23 -0600

    Finished Styling Footer icons

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -11,13 +11,13 @@ function Footer() {
         <a href="" className="load">
           <i class="fas fa-retweet fa-2x" />
         </a>
-        <p className="chatNumber">6</p>
+        <p className="number">6</p>
       </div>
       <div className="grouped item">
         <a href="" className="heart">
           <i class="far fa-heart fa-2x" />
         </a>
-        <p className="loveNumber ">4</p>
+        <p className="number ">4</p>
       </div>
       <a href="" className="envelope item">
         <i class="far fa-envelope fa-2x" />
