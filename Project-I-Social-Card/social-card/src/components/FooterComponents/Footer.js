03b158301ff08c9adc6142d54ef31ec28d47b85e
Robert Pedersen <rob84044@users.noreply.github.com>
2019-05-09 18:47:38 -0600

    Added the footer JSX
    I also started styling the icons and realize that I am going to need to change the A tag that is my app.js file in order to have any JS event listeners occur

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -2,7 +2,28 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
-  return <div className="Footer" />;
+  return (
+    <div className="footer">
+      <a href="" className="chat item">
+        <i class="far fa-comments fa-2x" />
+      </a>
+      <div className="grouped item">
+        <a href="" className="load">
+          <i class="fas fa-retweet fa-2x" />
+        </a>
+        <p className="chatNumber">6</p>
+      </div>
+      <div className="grouped item">
+        <a href="" className="heart">
+          <i class="far fa-heart fa-2x" />
+        </a>
+        <p className="loveNumber ">4</p>
+      </div>
+      <a href="" className="envelope item">
+        <i class="far fa-envelope fa-2x" />
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
