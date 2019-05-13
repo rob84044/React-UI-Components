7da6f71687368bc442fb6c3fda1628e9508d713d
Robert Pedersen <rob84044@users.noreply.github.com>
2019-05-12 18:52:22 -0600

    Styled the display section of the calc project

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Display.css';
 
 const Display = props => {
-  return <div className={props.style}>{props.text}</div>;
+  return (
+    <div className={props.style}>
+      <span className="text">{props.text}</span>
+    </div>
+  );
 };
 
 export default Display;
